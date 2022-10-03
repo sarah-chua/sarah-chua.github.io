8353ed8ca10024b9fd3d76ee913b3760ab8556c2
Sarah Chua <chuawxsarah@gmail.com>
2022-10-03 10:21:43 +0800

    p5js grid circles

--- index.js
+++ index.js
@@ -17,26 +17,29 @@ function setup() {
 
     var cnv = createCanvas(width, height);
     cnv.parent("big-section");
-    dim = width / 2;
+    dimX = width / 3;
+    dimY = height / 2;
     background(0);
     colorMode(HSB, 360, 100, 100);
     noStroke();
     ellipseMode(RADIUS);
-    frameRate(1);
+    frameRate(2);
 }
 
 function draw() {
     background(0);
-    for (let x = 0; x <= width; x += dim) {
-        drawGradient(x, height / 2);
+    for (let x = 0; x <= width; x += dimX) {
+        for (let y = 0; y <=height; y += dimY){
+            drawGradient(x,y);
+        }
     }
 }
   
 function drawGradient(x, y) {
-    let radius = dim / 2;
-    let h = random(0, 360);
+    let radius = dimY / 2; // radius is half of diameter
+    let h = random(0, 280); // set hue
     for (let r = radius; r > 0; --r) {
-        fill(h, 90, 90);
+        fill(h, 70, 80);
         ellipse(x, y, r, r);
         h = (h + 1) % 360;
     }
