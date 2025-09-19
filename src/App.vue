7aef8dfe9dc2a279fbd019104ca75e6ad6663a7f
Sarah Chua <sarahchua@Sarahs-MacBook-Pro.local>
2025-09-19 16:26:36 +0800

    layout: add logo to nav to home

--- src/App.vue
+++ src/App.vue
@@ -2,7 +2,31 @@
 </script>
 
 <template>
+  <div id="sticky-logo" @click="$router.push('/')">
+    <p>Sarah Chua</p>
+  </div>
   <RouterView />
 </template>
 
-<style scoped></style>
+<style scoped>
+#sticky-logo {
+  position: sticky;
+  top: 0;
+  background-color: #FFF9CE;
+  padding: 2%;
+  border-bottom: 2px dashed black;
+  z-index: 2;
+
+  p {
+    font-family: SarahBlock, sans-serif;
+    font-size: 2rem;
+    margin: 0px;
+  }
+}
+
+@media only screen and (min-width: 1024px) {
+  #sticky-logo {
+    padding: 1%;
+  }
+}
+</style>
